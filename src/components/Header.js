460e386d20c4561744bc2c35b2b5f58538ab9c5a
unknown <iemamyaman007@gmail.com>
2022-03-03 01:08:19 +0530

    bg & logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import React from "react"
 export default function Header(){
     return(
         <div className="header">
-            <h1>MEME GENERATOR</h1>
-            <p>React Course Project 3</p>
+            <img 
+                src ="logo.png"
+                className="header-logo"
+            />
         </div>
     )
 }
