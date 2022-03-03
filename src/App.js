11ad96eec23dc6090bff2d1b2f3d953e40499d9b
unknown <iemamyaman007@gmail.com>
2022-03-03 14:24:05 +0530

    deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Meme from "./components/Meme"
 
 export default function App(){
     return(
-        <Header/>
+        <div>
+            <Header/>
+            <Meme/>
+        </div>
     )
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,3 +1,14 @@
 import React from "react"
 
-export default function Meme(){}
+export default function Meme(){
+    return(
+        <div className="meme--box">
+            <div className="meme--text">
+                <input type ="text" placeholder="Top Text" name ="toptext" id ="top--text"></input>
+                <input type ="text" placeholder="Bottom Text" name ="bottomtext" id ="bottom--text"></input>
+                <button id ="submit" type ="submit">Change image</button>
+            </div>
+            <div className="meme--image"></div>
+        </div>
+    )
+}
