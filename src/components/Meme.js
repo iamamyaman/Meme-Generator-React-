9a4169d18c565007e095989106c27bedfb72e361
unknown <iemamyaman007@gmail.com>
2022-03-03 20:09:49 +0530

    added all functionality

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,29 +1,78 @@
 import React from "react"
 
 export default function Meme(){
+    const [meme,setMeme] = React.useState({
+        topText:"",
+        bottomText:"",
+        randomImage:"http://i.imgflip.com/1bij.jpg"
+    });
+
+    const [allMemes,setAllMemes] = React.useState([]);
+    
+    React.useEffect(() => {
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
+    },[])
+    
+
+    function getRandomImage(){
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        console.log(url)
+        setMeme(prevMeme=>({
+            ...prevMeme,
+            randomImage:url
+            }))
+
+    }
+
+    function handleChange(event){
+         const{name,value} = event.target
+         setMeme(prevMeme=>
+             ({
+                 ...prevMeme,
+                 [name]:value
+            })
+         )
+    }
+
+
     return(
         <div className="meme--box">
                 <div class ="input--area">
                     <input 
                       type ="text"
                       placeholder="Top Text" 
-                      name ="toptext" 
+                      name ="topText" 
                       className="top--text"
+                      value={meme.topText}
+                      onChange={handleChange}
                       />
+
                     <input 
                       type ="text"
                       placeholder="Bottom Text" 
-                      name ="bottomtext"
+                      name ="bottomText"
                       id ="bottom--text"
+                      value={meme.bottomText}
+                      onChange={handleChange}
                       />
+
                     <button 
+                      onClick={getRandomImage}
                       id ="submit" 
                       type ="submit">
                           Change image
                       </button>
                 </div>
                 <div className="meme--image">
-                    <img src="http://i.imgflip.com/1bij.jpg"></img>
+                    <img src={meme.randomImage} className ="meme--image"></img>
+                    <h1 className ="meme--text top">{meme.topText}</h1>
+                    <h1 className ="meme--text bottom">{meme.bottomText}</h1>
                 </div>
         </div>
     )
