64d88e74e91d92cbce7f046e80774baaed0b3a51
unknown <iemamyaman007@gmail.com>
2022-03-03 17:47:39 +0530

    ..

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,12 +3,28 @@ import React from "react"
 export default function Meme(){
     return(
         <div className="meme--box">
-            <div className="meme--text">
-                <input type ="text" placeholder="Top Text" name ="toptext" id ="top--text"></input>
-                <input type ="text" placeholder="Bottom Text" name ="bottomtext" id ="bottom--text"></input>
-                <button id ="submit" type ="submit">Change image</button>
-            </div>
-            <div className="meme--image"></div>
+                <div class ="input--area">
+                    <input 
+                      type ="text"
+                      placeholder="Top Text" 
+                      name ="toptext" 
+                      className="top--text"
+                      />
+                    <input 
+                      type ="text"
+                      placeholder="Bottom Text" 
+                      name ="bottomtext"
+                      id ="bottom--text"
+                      />
+                    <button 
+                      id ="submit" 
+                      type ="submit">
+                          Change image
+                      </button>
+                </div>
+                <div className="meme--image">
+                    <img src="http://i.imgflip.com/1bij.jpg"></img>
+                </div>
         </div>
     )
 }
